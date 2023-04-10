a235db6416c92bbf4b6491a5e37d9ac6fc037327
atheo810 <atheos810@gmail.com>
2023-04-10 16:52:24 +0800

    berhasil koneksi dengan .env

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const router = require("./routers");
 
+dotenv.config();
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
+
 app.listen(process.env.PORT, () =>
   console.log("Server is running on port http://localhost:3000")
 );
