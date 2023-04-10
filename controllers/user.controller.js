b000387531e71b6b888af627704b63a7be4c967a
atheo810 <atheos810@gmail.com>
2023-04-10 17:23:54 +0800

    berhasil membuat crud

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,8 +4,63 @@ const prisma = new PrismaClient();
 class userController {
   static async getAllUsers(req, res) {
     try {
-      const users = await prisma.user.findMany({});
-      res.status(200).json(users);
+      const result = await prisma.User.findMany({});
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+  static async createUser(req, res) {
+    try {
+      const result = await prisma.User.create({
+        data: {
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        },
+      });
+      res.status(201).json(result);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+  static async getUserById(req, res) {
+    try {
+      const result = await prisma.User.findUnique({
+        where: {
+          id: parseInt(req.params.id),
+        },
+      });
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+  static async updateUser(req, res) {
+    try {
+      const result = await prisma.User.update({
+        where: {
+          id: parseInt(req.params.id),
+        },
+        data: {
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        },
+      });
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+  static async deleteUser(req, res) {
+    try {
+      const result = await prisma.User.delete({
+        where: {
+          id: Number(req.params.id),
+        },
+      });
+      res.status(200).json(result);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
--- routers/user.router.js
+++ routers/user.router.js
@@ -4,4 +4,8 @@ const userController = require("../controllers/user.controller");
 const router = Router();
 
 router.get("/users", userController.getAllUsers);
+router.get("/users/:id", userController.getUserById);
+router.post("/users", userController.createUser);
+router.put("/users/:id", userController.updateUser);
+router.delete("/users/:id", userController.deleteUser);
 module.exports = router;
